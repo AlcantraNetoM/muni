230923cfd1f8d9fab68015122dbe05a9e84da77f
Alcantra Neto Manuel <126702437+AlcantraNetoM@users.noreply.github.com>
2025-11-19 20:10:56 +0000

    act

---
+++ app/sobre/page.tsx
@@ -0,0 +1,13 @@
+export default function page() {
+    return (
+        <>
+            <h2>Desenvolvimento Web Moderno </h2>
+            <ul>
+                <li>React e Next.js revolucionaram a criação de interfaces web.</li>
+                <li>São usados por empresas com Facbook, Netflix e Airbnb.</li>
+                <li>Dominar estas tecnologias abre muitas oportunidades!</li>
+            </ul>
+        </>
+
+    )
+} 
