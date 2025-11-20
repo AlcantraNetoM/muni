ceedaf208336283a60678481ff74ff1fb4f8e33c
Alcantra Neto Manuel <126702437+AlcantraNetoM@users.noreply.github.com>
2025-11-20 10:55:18 +0000

    act

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,7 +1,13 @@
-// deve importar o componente
-import Link from 'next/link'
+import Link from "next/link";
 
+export default function Counter() {
+    return (
+        <div>
+            <h2>Counter</h2>
 
-<Link href="/counter">
-    Counter
-</Link>
+            <Link href="/counter">
+                Counter
+            </Link>
+        </div>
+    );
+}
