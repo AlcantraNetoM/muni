16652c442b25b747f7e54651a5668407866a160f
Alcantra Neto Manuel <126702437+AlcantraNetoM@users.noreply.github.com>
2025-11-20 10:58:20 +0000

    act

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -1,13 +1,9 @@
 import Link from "next/link";
 
 export default function Counter() {
-    return (
-        <div>
-            <h2>Counter</h2>
-
-            <Link href="/counter">
-                Counter
-            </Link>
-        </div>
-    );
+  return (
+    <div>
+      <Link href="/counter">Counter</Link>
+    </div>
+  );
 }
